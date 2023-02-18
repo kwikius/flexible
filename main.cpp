9cbf40eaafc33c0f5bb7ca57fd275daa4630dfe3
Andy <kwikius@yahoo.com>
2023-02-18 10:04:14 +0000

    update from dormant

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int print_main (int argc, char *argv[]);
 int test_main (int argc, char *argv[]);
 
 int main( int argc, char *argv[]){
-   //return print_main(argc,argv);
-   return test_main(argc,argv);
+   return print_main(argc,argv);
+ //  return test_main(argc,argv);
 }
 
 
